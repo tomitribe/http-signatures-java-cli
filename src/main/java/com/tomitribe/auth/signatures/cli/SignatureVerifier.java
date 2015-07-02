3323c4e9c238796e42d1c90338e92fba60662b5e
Romain Manni-Bucau <rmannibu@starbucks.com>
2015-07-02 10:55:54 -0700

    support method parameter, reusing cxf integration to avoid to zrite the technical code again

--- src/main/java/com/tomitribe/auth/signatures/cli/SignatureVerifier.java
+++ src/main/java/com/tomitribe/auth/signatures/cli/SignatureVerifier.java
@@ -9,28 +9,21 @@
  */
 package com.tomitribe.auth.signatures.cli;
 
+import com.tomitribe.auth.signatures.cxf.feature.SecurityFeature;
 import org.apache.cxf.configuration.jsse.TLSClientParameters;
+import org.apache.cxf.feature.LoggingFeature;
 import org.apache.cxf.jaxrs.client.WebClient;
 import org.apache.cxf.transport.http.HTTPConduit;
-import org.tomitribe.auth.signatures.Base64;
-import org.tomitribe.auth.signatures.Signature;
-import org.tomitribe.auth.signatures.Signer;
 import org.tomitribe.util.IO;
 
-import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 import javax.ws.rs.core.Response;
 import java.io.InputStream;
-import java.security.MessageDigest;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
+import java.util.Arrays;
+import java.util.Collections;
 
 /**
  * Description.
@@ -52,40 +45,22 @@ public class SignatureVerifier {
         final String alias = args[1];
         final String address = args[2];
         final String path = args[3];
-        final String method = args[4];
-        final String payload;
-        if (args.length == 6) {
-            payload = args[5];
-        } else {
-            payload = "";
-        }
 
         System.out.println("secret = " + secret);
         System.out.println("alias = " + alias);
         System.out.println("address = " + address);
         System.out.println("path = " + path);
-        System.out.println("method = " + method);
-        System.out.println("input = " + payload);
-
-        final String today = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz", Locale.US).format(new Date());
-        final String digest =
-                "SHA=" + new String(Base64.encodeBase64(MessageDigest.getInstance("SHA1").digest(payload.getBytes())));
-
-        final SecretKey secretKey = new SecretKeySpec(secret.getBytes(), "hmacSHA256");
-        final Signature signature = new Signature(alias, "hmac-sha256", null, "(request-target)", "digest", "date");
-        final Map<String, String> headers = new HashMap<>();
-        headers.put("Date", today);
-        headers.put("Digest", digest);
-
-        final Signature sign = new Signer(secretKey, signature).sign(method, path, headers);
-        System.out.println(sign);
 
         WebClient webClient = WebClient
-                .create(address)
-                .path(path)
-                .header("Authorization", sign)
-                .header("Date", today)
-                .header("Digest", digest);
+                .create(address, Collections.emptyList(),
+                    Arrays.asList(new SecurityFeature(
+                        "sha-256",
+                        secret,
+                        alias,
+                        "hmac-sha256",
+                        "(request-target) date digest"
+                    ), new LoggingFeature()), null)
+                .path(path);
 
         final HTTPConduit conduit = WebClient.getConfig(webClient).getHttpConduit();
 
@@ -105,7 +80,7 @@ public class SignatureVerifier {
         params.setDisableCNCheck(true);
         conduit.setTlsClientParameters(params);
 
-        final Response response = webClient.get();
+        final Response response = webClient.invoke(args[4], args.length == 6 ? args[5] : null);
         System.out.println(response.getStatus());
         System.out.println(IO.slurp(InputStream.class.cast(response.getEntity())));
     }
